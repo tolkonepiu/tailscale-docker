905d2e5ec9b168c0ec800d2b92471be124e03bff
Pavel Popov <tolkonepiu@users.noreply.github.com>
2022-04-28 01:46:28 +0300

    Fix tailscale script

--- run.sh
+++ run.sh
@@ -30,7 +30,7 @@ else
 fi
 
 echo "Starting tailscaled"
-tailscaled "${TAILSCALED_ARGS}" &
+tailscaled ${TAILSCALED_ARGS} &
 PID=$!
 
 UP_ARGS="--accept-dns=false"
@@ -45,7 +45,7 @@ if [ -n "${EXTRA_ARGS}" ]; then
 fi
 
 echo "Running tailscale up"
-tailscale --socket=/tmp/tailscaled.sock up "${UP_ARGS}"
+tailscale --socket=/tmp/tailscaled.sock up ${UP_ARGS}
 
 if [ -n "${DEST_IP}" ]; then
   echo "Adding iptables rule for DNAT"
